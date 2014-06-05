50c8ee21b4dd5926dd33bd3f23c988d8d7382ca6
Adam <amarinelli@esri.ca>
2014-06-05 17:38:49 -0400

    Further along, more functionality
    - Added Class with starting properties and txt file open function
    - parsing txt file based on patterns and comma separators
    - two arrays holding parsed and formatted "requires" and "objects"
    - next step, formatting output, further error checking and pattern
    matching

--- import_helper.py
+++ import_helper.py
@@ -1,21 +1,47 @@
 import re
 
-imports_file = open(r".\import_statements.txt")
-count = 0
-items = []
-p_alpha_num = re.compile("\w") #Matches any alphanumeric character; [a-zA-Z0-9_].
-p_non = re.compile("\W") #Matches any non-alphanumeric character; [^a-zA-Z0-9_].
+class Formatter:
+    def __init__(self):                
+        self.count = 0
+        self.items = []
+        self.requires = []
+        self.objects = []
+        self.p_alpha_num = re.compile("\w") #Matches any alphanumeric character; [a-zA-Z0-9_].
+        self.p_non = re.compile("\W") #Matches any non-alphanumeric character; [^a-zA-Z0-9_].
 
-for line in imports_file:
+    def open_file(self, file_name):
+        import_file = open(file_name)
+        return import_file
+
+#Instantiate
+formatter = Formatter()
+txtFile = formatter.open_file(r".\import_statements.txt")
+
+for line in txtFile:
     s = line.strip()
-    for item in (s.split(",")):
-        if (item.strip() != ""):
-            items.append(item.strip())
-    count+=1
+    for values in (s.split(",")):
+        if (values.strip() != ""):
+            formatter.items.append(values.strip())
+
+for item in formatter.items:    
+    if (item.startswith("\"") and item.endswith("\"")):        
+        formatter.requires.append(item)
+
+for require in formatter.requires:
+    formatter.items.remove(require)
     
-for it in items:
-    print it
-    
-#print count
+for obj in formatter.items:    
+    if (formatter.p_non.match(obj[0]) or formatter.p_non.match(obj[-1])):
+        continue
+    else:
+        formatter.objects.append(obj)
+
+for y in formatter.requires:
+    print y
+
+for z in formatter.objects:
+    print z
+
+txtFile.close()
     
     
