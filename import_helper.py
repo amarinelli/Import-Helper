6e1a504293b2282df3427d20dbc1041743307f9a
Adam <amarinelli@esri.ca>
2014-06-06 17:59:15 -0400

    Building function to match requires to variables

--- import_helper.py
+++ import_helper.py
@@ -11,8 +11,25 @@ class Formatter:
         import_file = open(file_name)
         return import_file
 
-    def find_solo(self, item):
-        formatter.requires[item].rsplit("/", 1)[1][:-1]
+    def find_solo(self, test_requires, test_objects):        
+        test_list = test_requires
+        dont_exists = []
+        exists = bool
+        for g in range(len(test_list)-1):
+            test_require = test_list[g].rsplit("/", 1)[1][:-1]
+            for test_obj in test_objects:
+                print "TESTING:  " + test_require + "  against  " + test_obj
+                if test_obj.lower() == test_require.lower():
+                    print "matched"
+                    dont_exists.append(True)
+                    break
+                else:
+                    dont_exists.append(False)
+        print dont_exists
+        if (exists == False):
+            print "trying to remove:  " + test_require 
+            del self.requires[g]
+        return
 
 #Instantiate
 formatter = Formatter()
@@ -45,19 +62,23 @@ for obj in formatter.items:
 
 if ((len(formatter.requires)-1) != len(formatter.objects)):
     #need to handle items like BorderContainer which are not passed into a variable
-    extra = []
-    for g in range(len(formatter.requires)):        
-        if (formatter.requires[g].rsplit("/", 1)[1][:-1] == "BorderContainer"):
-            extra.append(formatter.requires[g])            
-        elif (formatter.requires[g].rsplit("/", 1)[1][:-1] == "ContentPane"):
-            extra.append(formatter.requires[g])
-            
-    formatter.requires.remove("\"dijit/layout/BorderContainer\"")
-    formatter.requires.remove("\"dijit/layout/ContentPane\"")
+    formatter.find_solo(formatter.requires, formatter.objects) 
 
-    for f in range(len(formatter.objects)):
+
+
+##        if (formatter.requires[g].rsplit("/", 1)[1][:-1] == "BorderContainer"):
+##            extra.append(formatter.requires[g])            
+##        elif (formatter.requires[g].rsplit("/", 1)[1][:-1] == "ContentPane"):
+##            extra.append(formatter.requires[g])
+##    
+##    formatter.requires.remove("\"dijit/layout/BorderContainer\"")
+##    formatter.requires.remove("\"dijit/layout/ContentPane\"")
+
+        
+
+    for f in range(len(formatter.objects)):        
         print "import {0} = require({1});".format(formatter.objects[f], formatter.requires[f])
-
+        
 else:
     #requires and objects arrays match except for "dojo/domReady!" in requires
     for f in range(len(formatter.objects)):
