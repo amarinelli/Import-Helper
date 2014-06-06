b3b7cc79ddba645798a8dceaaf24e0259d841a4f
Adam <amarinelli@esri.ca>
2014-06-06 16:05:40 -0400

    Further handling different scenarios and patterns
    - Works well for simple samples
    - Working to recognize requires which are not loaded into variables

--- import_helper.py
+++ import_helper.py
@@ -2,16 +2,17 @@ import re
 
 class Formatter:
     def __init__(self):                
-        self.count = 0
         self.items = []
         self.requires = []
         self.objects = []
-        self.p_alpha_num = re.compile("\w") #Matches any alphanumeric character; [a-zA-Z0-9_].
         self.p_non = re.compile("\W") #Matches any non-alphanumeric character; [^a-zA-Z0-9_].
 
     def open_file(self, file_name):
         import_file = open(file_name)
         return import_file
+
+    def find_solo(self, item):
+        formatter.requires[item].rsplit("/", 1)[1][:-1]
 
 #Instantiate
 formatter = Formatter()
@@ -36,11 +37,31 @@ for obj in formatter.items:
     else:
         formatter.objects.append(obj)
 
-for y in formatter.requires:
-    print y
+##for y in formatter.requires:
+##    print y
+##
+##for z in formatter.objects:
+##    print z
 
-for z in formatter.objects:
-    print z
+if ((len(formatter.requires)-1) != len(formatter.objects)):
+    #need to handle items like BorderContainer which are not passed into a variable
+    extra = []
+    for g in range(len(formatter.requires)):        
+        if (formatter.requires[g].rsplit("/", 1)[1][:-1] == "BorderContainer"):
+            extra.append(formatter.requires[g])            
+        elif (formatter.requires[g].rsplit("/", 1)[1][:-1] == "ContentPane"):
+            extra.append(formatter.requires[g])
+            
+    formatter.requires.remove("\"dijit/layout/BorderContainer\"")
+    formatter.requires.remove("\"dijit/layout/ContentPane\"")
+
+    for f in range(len(formatter.objects)):
+        print "import {0} = require({1});".format(formatter.objects[f], formatter.requires[f])
+
+else:
+    #requires and objects arrays match except for "dojo/domReady!" in requires
+    for f in range(len(formatter.objects)):
+        print "import {0} = require({1});".format(formatter.objects[f], formatter.requires[f])
 
 txtFile.close()
     
